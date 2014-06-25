191802047cb9c1a8bf1624883038ec1c0bfe33f8
maggarwal <maggarwal@amplify.com>
2014-06-25 17:46:31 -0400

    minor cleanup

--- t.py
+++ t.py
@@ -8,17 +8,11 @@ class Todo:
 
     """
     This class maintains the Todo file and all the supported methods
-    it supportes the following
-    __init__ : which does all the config files settings and stuff
-    add() it adds one task to the file
-    parse() it parses the task for date and the message
-    remove() it removes a task (marks as finished)
     """
 
     def __init__(self, config_file_path=None):
         """This takes in a config file (json) parses it
         and does the initilization
-
 
         :config_file_path: it takes in a path to config_file
            - (default cwd/tconfig.json)
@@ -34,9 +28,6 @@ class Todo:
             self.config = json.load(config_file)
             self.todo_path = os.path.abspath(self.config['todo_path'])
             self.todo_file = self.todo_path + '/' + self.config['todo_file']
-            print self.todo_file
-            # import pprint; pprint.pprint(self.config)
-            # print self.config['asdf']
 
     def add_task(self, task):
         """adds a task to the list
@@ -56,13 +47,12 @@ class Todo:
         task = task + '\n'
         # the inplace redirects the stdout to file
         # so whatever we are printing is going there
+        # after this loop file closes so we can print anything
         for line in fileinput.input(self.todo_file, inplace=True):
             if task == line:
                 continue
-
             # trailing comma with print omits the new line char
             print line,
-        # now the file is closed so we can safely print
 
 
 def main():
